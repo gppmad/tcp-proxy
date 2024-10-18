a28b3abf9b663685281a701d9e09a83df29b7c8f
Giuseppe Maddiona <giuseppe.maddiona@gmail.com>
2024-10-18 14:50:03 +0200

    first version of the proxy

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # tcp-proxy
-Simple TCP Proxy built with Golang
+Simple TCP Proxy built with Golang.
+
+It is specialized to forward the request to httbin.org and return the response.
+
+## Usage
+Run the proxy with the following command:
+```bash
+go run main.go
+```
+
+## Example
+Open a new terminal and send a request to the proxy:
+
+```bash
+curl -X GET http://localhost:8080/get
+```
+
+
+
--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"log"
+	"net"
+)
+
+func handleConnection(clientConn net.Conn, targetAdd string) {
+	defer clientConn.Close()
+
+	targetConn, err := net.Dial("tcp", targetAdd)
+	if err != nil {
+		log.Printf("Failed to connect to target server", targetAdd)
+	}
+	defer targetConn.Close()
+
+	go io.Copy(targetConn, clientConn)
+	io.Copy(clientConn, targetConn)
+
+}
 
 func main() {
-	fmt.Println("Proxy is now running")
+	listenAddr := "localhost:8080"
+	targetAddr := "httpbin.org:80"
+
+	listener, err := net.Listen("tcp", listenAddr)
+	if err != nil {
+		log.Fatal("Failed to listen on %s \n", listenAddr, err)
+	}
+	defer listener.Close()
+
+	log.Println("Proxy is now listening on", listenAddr)
+	for {
+		clientConn, err := listener.Accept()
+		if err != nil {
+			log.Println("Failed to accept connection", err)
+			continue
+		}
+		go handleConnection(clientConn, targetAddr)
+	}
+
 }
